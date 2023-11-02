e7c2e3a3a65be97f9aa61dd9821de8c8aac0ed26
MaxxBahr <schnee.bahr@googlemail.com>
2023-11-02 20:10:05 +0100

    added post methods and sending json via post

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const PORT = 3000;
 
+const people = [
+    {
+        "name": "Caleb",
+        "industry": "Music"
+    },
+    {
+        "name": "John",
+        "industry": "networking"
+    },
+    {
+        "name": "Sal",
+        "industry": "Sports medicine"
+    }
+]
+
 app.get("/", (req, res) =>{
-    res.send("hello world");
+    res.send("welcome on the homepage");
+})
+
+app.get("/api/customers", (req, res) =>{
+    res.send({"data":people});
+})
+
+app.post("/api/customers", (req, res) =>{
+    console.log(req.body);
+    res.send(req.body);
 })
 
 app.post("/", (req, res) =>{
-    res.send("this is a POST request");
+    res.send({"data":people});
 })
 
 app.listen(PORT, () => {
